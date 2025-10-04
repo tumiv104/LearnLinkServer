c1e5e7644150d5873ab23bf7854089af58168d25
Fadetz602 <chuphucanh602@gmail.com>
2025-10-04 10:07:12 +0700

    Create Child, Update Profile
    Create Child, Update Profile

--- API/Controllers/User/ParentController.cs
+++ API/Controllers/User/ParentController.cs
@@ -1,5 +1,7 @@
 ﻿using API.Controllers;
+using Application.DTOs.User;
 using Application.Interfaces;
+using Application.Interfaces.Common;
 using Application.Interfaces.User;
 using Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -31,5 +33,29 @@ public class ParentController : BaseController
         var children = await _parentService.GetChildrenAsync(parentId);
         return OkResponse(children, "List of your children");
     }
+
+    [HttpPost("children")]
+    [Authorize(Roles = "Parent")]
+    public async Task<IActionResult> CreateChild([FromForm] ChildCreateDTO childDTO, [FromServices] IFileStorage fileStorage, [FromServices] IWebHostEnvironment env)
+    {
+        var parentIdClaim = User.FindFirstValue("id");
+        if (string.IsNullOrEmpty(parentIdClaim))
+            return UnauthorizedResponse();
+
+        var parentId = int.Parse(parentIdClaim);
+
+        // Upload avatar nếu có
+        if (childDTO.AvatarFile != null)
+        {
+            using var stream = childDTO.AvatarFile.OpenReadStream();
+            var url = await fileStorage.SaveAsync(stream, childDTO.AvatarFile.FileName, "avatars", env.WebRootPath);
+            childDTO.AvatarUrl = url;
+        }
+        var success = await _parentService.CreateChildAsync(parentId, childDTO);
+        if (!success) return BadRequestResponse("Failed to create child");
+
+        return OkResponse<object>(null, "Child created successfully");
+    }
+
 
 }
